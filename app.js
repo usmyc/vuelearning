0d8804245766fa3e2bbcd2c51e90738f5688bf49
usmyc <avusameyuce@outlook.com>
2023-01-26 11:12:55 +0100

    more

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ import { createApp } from "vue";
 var title = "Hello World";
 
 async function getData() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
   const object = await response.json();
-  title = object.title;
+  title = object;
 }
 getData().then(() => {
   createApp({
     data() {
-      console.log(title);
       return {
         message: "Hello AI!",
         weather: "sunny",
@@ -17,6 +16,24 @@ getData().then(() => {
         x: 0,
         y: 0,
         title: title,
+        books: [
+          {
+            title: "name of the wind",
+            author: "patrick rothfuss",
+            isFav: true,
+          },
+          {
+            title: "the way of kings",
+            author: "brandon sanderson",
+            isFav: false,
+          },
+          {
+            title: "the final empire",
+            author: "brandon sanderson",
+            isFav: true,
+          },
+        ],
+        url: "https://www.yuce.dev/",
       };
     },
     methods: {
@@ -33,7 +50,15 @@ getData().then(() => {
         this.x = e.screenX;
         this.y = e.screenY;
       },
+      isFavhandler(book) {
+        book.isFav = !book.isFav;
+      },
     },
+    computed:{
+      filteredBooks(){
+        return this.books.filter((book)=>book.isFav)
+      }
+    }
   }).mount("#app");
 });
 
